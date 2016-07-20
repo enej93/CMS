b0e256f06205387c93980c6410f755cf8133d788
enej93 <enej.licina@windowslive.com>
2016-07-20 17:36:09 +0200

    minor code update

--- app/partial/inventory/inventory.js
+++ app/partial/inventory/inventory.js
@@ -1,4 +1,45 @@
 angular.module('app').controller('InventoryCtrl',function($scope){
 
+    /*(function() {
+
+        window.inputNumber = function(el) {
+
+            var min = el.attr('min') || false;
+            var max = el.attr('max') || false;
+
+            var inventoryNumber = {};
+
+            inventoryNumber.dec = el.prev();
+            inventoryNumber.inc = el.next();
+
+            el.each(function() {
+                init($(this));
+            });
+
+            function init(el) {
+
+                inventoryNumber.dec.on('click', decrement);
+                inventoryNumber.inc.on('click', increment);
+
+                function decrement() {
+                    var value = el[0].value;
+                    value--;
+                    if(!min || value >= min) {
+                        el[0].value = value;
+                    }
+                }
+
+                function increment() {
+                    var value = el[0].value;
+                    value++;
+                    if(!max || value <= max) {
+                        el[0].value = value++;
+                    }
+                }
+            }
+        };
+    })();
+
+    inputNumber($('.input-number')); */
 
 });
